381c877b88969a1521401fe6e13760aa7bca1dd5
alexandrakraeva <66311877+alexandrakraeva@users.noreply.github.com>
2023-10-19 01:39:13 +0200

    added files

---
+++ script.js
@@ -0,0 +1,27 @@
+﻿const playButton = document.getElementById('play-button');
+const audio = document.getElementById('audio');
+const background = document.getElementById('background');
+
+let isPlaying = false;
+
+playButton.addEventListener('click', () => {
+    if (!isPlaying) {
+        // Trigger audio playback in response to a user interaction
+        audio.play();
+
+        isPlaying = true;
+        background.style.background = 'linear-gradient(45deg, yellow, red)'; // Set gradient when playing
+        playButton.classList.remove('playing-off');
+
+        // Restore the animation class to play the gradient animation
+        background.style.animation = 'gradient-animation 4s infinite alternate';
+    } else {
+        audio.pause();
+        isPlaying = false;
+        background.style.background = 'black'; // Set background to black when stopped
+        playButton.classList.add('playing-off');
+
+        // Remove the animation class to stop the gradient animation
+        background.style.animation = 'none';
+    }
+});
---
+++ scriptt.js
@@ -0,0 +1,31 @@
+﻿
+
+
+
+
+const playButton = document.getElementById('play-button');
+const audio = document.getElementById('audio');
+const background = document.getElementById('background');
+
+let isPlaying = false;
+
+playButton.addEventListener('click', () => {
+    if (!isPlaying) {
+        // Trigger audio playback in response to a user interaction
+        audio.play();
+
+        isPlaying = true;
+        playButton.classList.remove('playing-off');
+        // Set opacity to 1 to reveal the gradient background (fade in)
+        background.style.opacity = '1';
+    } else {
+        audio.pause();
+        isPlaying = false;
+        playButton.classList.add('playing-off');
+        // Set opacity to 0 to hide the gradient background (fade out)
+        background.style.opacity = '0';
+    }
+});
+
+
+
